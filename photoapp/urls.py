cafc245a28069cb11466c4363deaaa6549235672
HassibIbrahim <ihassib@gmail.com>
2018-12-11 14:39:27 +0300

    location preferance added

--- photoapp/urls.py
+++ photoapp/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns=[
     url(r'^$', views.show_categories, name='imagesHomeCategory'),
     url(r'^search/', views.search_results, name='search_results'),
+        url(r'^$', views.my_locations, name='location_results'),
     url(r'^location/', views.my_locations, name='location_results')
 
 ]
--- photoapp/views.py
+++ photoapp/views.py
@@ -5,12 +5,17 @@ from .models import  *
 # Create your views here.
 def show_categories(request):
     # categories=Category.all_categories()
+    locations = Location.objects.all()
+
 
     categories = Category.objects.all()
     images = Image.all_images()
     # imagescategory = Image.show_by_category(category=ca)
     if request.GET.get("category"):
         images = Image.show_by_category(request.GET.get("category"))
+    elif request.GET.get("location"):
+        images = Image.show_by_location(request.GET.get("location"))
+
     else:
         images= Image.all_images()
 
@@ -21,7 +26,7 @@ def show_categories(request):
     # if request.GET.get("category")):
     #     images = Image.filter_by_category(request.GET.get("category"))
 
-    return render(request, 'images/homepage.html', {"categories":categories, "images":images })
+    return render(request, 'images/homepage.html', {"categories":categories, "locations":locations,"images":images })
 
 #
 # def images(request):
